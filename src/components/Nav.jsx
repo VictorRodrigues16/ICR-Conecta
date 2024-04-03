970e06cb7ecacfe0e85be52f1117d2314a4c48f8
Felipe <felipe.salazar2004@outlook.com>
2024-04-03 16:02:15 -0300

    Feat: burger menu / Fix: Forms

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,35 +1,50 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "../styles/Nav.scss";
 import img from "../assets/nav-icon.svg";
+import { TiThMenu } from "react-icons/ti";
+
+const MenuItem = ({ href, children }) => (
+    <a href={href}>
+        <li className="nav-li">{children}</li>
+    </a>
+);
 
 export default function Nav() {
-  return (
-    <>
-      <header>
-        <nav>
-          <div id="cont-nav">
-            <img id="icon-nav" src={img} alt="img-nav" />
-            <ul id="list-nav">
-              <a href="#Ourapp">
-                <li className="nav-li">Início</li>
-              </a>
-              
-              <a href="#discover">
-                <li className="nav-li">Sobre o ICR</li>
-              </a>
+    const [isOpen, setIsOpen] = useState(false);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-              <a href="#information">
-                <li className="nav-li">Blog</li>
-              </a>
+    const checkWindowSize = useCallback(() => {
+        setWindowWidth(window.innerWidth);
+    }, []);
 
-              <a href="#form">
-                <li className="nav-li">Contato</li>
-              </a>
-            </ul>
-          </div>
-          <button id="nav-button">BAIXE O APLICATIVO</button>
-        </nav>
-      </header>
-    </>
-  );
+    useEffect(() => {
+        window.addEventListener("resize", checkWindowSize);
+        return () => window.removeEventListener("resize", checkWindowSize);
+    }, [checkWindowSize]);
+
+    const toggleMenu = useCallback(() => {
+        if (windowWidth <= 500) {
+            setIsOpen(!isOpen);
+        }
+    }, [isOpen, windowWidth]);
+
+    return (
+        <header>
+            <nav>
+                <button onClick={toggleMenu} className="hamburguer-icon">
+                    <TiThMenu />
+                </button>
+                <div id="cont-nav">
+                    <img id="icon-nav" src={img} alt="img-nav" />
+                    <ul id="list-nav" className={isOpen ? "open" : "hidden"}>
+                        <MenuItem href="#Ourapp">Início</MenuItem>
+                        <MenuItem href="#info-app">Sobre o ICR</MenuItem>
+                        <MenuItem href="#information">Blog</MenuItem>
+                        <MenuItem href="#form">Contato</MenuItem>
+                    </ul>
+                </div>
+                <button id="nav-button">BAIXE O APLICATIVO</button>
+            </nav>
+        </header>
+    );
 }
